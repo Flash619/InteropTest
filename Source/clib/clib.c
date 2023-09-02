288f798c93182c628e76a968b6197039cc7217f5
Travis Truttschel <travis@truttschel.com>
2023-09-02 01:15:56 -0500

    Fixed broken clib.

--- Source/clib/clib.c
+++ Source/clib/clib.c
@@ -10,9 +10,9 @@ int test_interop(
     const void *data1,
     const size_t number4,
     const void *data3,
-    const size_t number5
-    const size_t number6
-    const size_t number7
+    const size_t number5,
+    const size_t number6,
+    const size_t number7,
     const uint32_t number8
 )
 {
@@ -25,8 +25,8 @@ int test_interop(
     
     printf("number5: %zu\n", number5);
     printf("number6: %zu\n", number6);
-    printf("number7: %zu\n", number6);
-    printf("number8: %u\n", number6);
+    printf("number7: %zu\n", number7);
+    printf("number8: %u\n", number8);
     
     return number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8;
 }
